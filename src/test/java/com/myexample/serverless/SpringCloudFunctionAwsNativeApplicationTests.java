708a7a6b0247c8bc81d4654d9911a6ab1d661cdd
t_ita <brett.tetsuji@gmail.com>
2020-11-29 13:52:16 +0900

    First Commit ; Create Project with Spring itializr

---
+++ src/test/java/com/myexample/serverless/SpringCloudFunctionAwsNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexample.serverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionAwsNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
